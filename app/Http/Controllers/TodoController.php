dde14e19d7a7dfd980cbf24dd08c802e5665862d
松田晴登 <haruto.matsuda@gmail.com>
2021-09-02 19:59:26 +0900

    修正内容が分かるコミットメッセージ

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $items = DB::select('select * from todos');
+        return view('index', ['items' => $items]);
+    }
+}
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <style>
+    body {
+      font-size:16px;
+      margin: 5px;
+    }
+    .content {
+      margin:10px; 
+    }
+    </style>
+  </head>
+  <body>
+    <h1>@yield('title')</h1>
+    <div class="content">
+    @yield('content')
+    </div>
+</body>
+</html>
