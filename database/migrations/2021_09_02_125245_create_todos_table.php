6af90f876438c914372041303649a2c88f6a3ff7
松田晴登 <haruto.matsuda@gmail.com>
2021-09-09 11:18:28 +0900

    修正内容が分かるコミットメッセージ

--- database/migrations/2021_09_02_125245_create_todos_table.php
+++ database/migrations/2021_09_02_125245_create_todos_table.php
@@ -11,7 +11,7 @@ class CreateTodosTable extends Migration
      *
      * @return void
      */
-   public function up()
+    public function up()
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,66 +27,114 @@
   .content { 
     width: 70%; 
     margin: 100px auto 0;
-    
-
   }
-  .content1{
-    width: 70%;
-    height: 200px;
-    background: #fff;
-    border-radius: 30px;
 
-  }
   .btn {
   display: block;
-  width: 160px;
-  padding: 15px;
+  width: 80px;
   text-align: center;
   text-decoration: none;
-  color: #000;
-  border:1px solid #000;
-  border-radius: 30px;
+  color: #9370DB;
+  border:2px solid #9370DB;
+  border-radius: 6px;
 }
 .btn:hover {
-  background: #000;
+  background: #9370DB;
   color: #fff;
 }
+.card {
+  margin: 250px auto 0;
+  width: 55%;
+  height: 420px;
+  background: #fff;
+  border-radius: 10px;
+      
+}
 
+.card__content {
+  padding: 30px;
+}
+
+.card__content-ttl {
+  font-size: 18px;
+  margin-bottom: 30px;
+  font-weight: 400;
+}
+
+.card__content-cat {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.form{
+  width: 60%;
+  margin: 20px auto;
+}
+.form-item {
+  padding: 24px 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.form-item-input {
+  border: 1px solid #ddd;
+  border-radius: 6px;
+  padding: 0 10px;
+  height: 35px;
+  width: 75%;
+  font-size: 18px;
+}
     
 </style>
 </head>
 
 <body>
-<a href="#" class="btn">ボタン</a>
+  <div class="card">
+    <div class="card__content">
+      <div class="card__content-cat">Todo List</div>
+      <form action="/create" method="POST">
+        <div class="form-item">
+          <input type="text" name="contact" class="form-item-input">
+          <input type="submit" name="submit" value="追加" class="btn"/>
+        </div>
+      </form>
+    </div>
+        <table >
+          <tr>
+            <th>作成日</th>
+            <th>タスク名</th>
+            <th>更新</th>
+            <th>削除</th>
+          </tr>
+          @foreach ($items as $item)
+          <tr>
+            <td>
+              {{$item->id}}
+            </td>
+            <td>
+              {{$item->content}}
+            </td>
+            <td>
+              <button>送信</button>
+              <a href="#" class="btn">ボタン</a>
+              {{$item->created_at}}
+            </td>
+            <td>
+              {{$item->updated_at}}
+            </td>
+          </tr>
+          @endforeach
+        </table>
+        <a href="#" class="btn">ボタン</a>
+
+      </div>
+    </div>
+  </div>
 
 
 
-<table>
-  <tr>
-    <th>作成日</th>
-    <th>タスク名</th>
-    <th>更新</th>
-    <th>削除</th>
-  </tr>
-  @foreach ($items as $item)
-  <tr>
-    <td>
-      {{$item->id}}
-    </td>
-    <td>
-      {{$item->content}}
-    </td>
-    <td>
-      <button>送信</button>
-      <a href="#" class="btn">ボタン</a>
-      {{$item->created_at}}
-    </td>
-    <td>
-      {{$item->updated_at}}
-    </td>
-  </tr>
-  @endforeach
-</table>
+
 
 
 
